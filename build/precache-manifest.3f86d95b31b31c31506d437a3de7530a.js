3c608b9be0381fb61edf2f1b11f8533501b52578
asv1997 <asvignesh1997@gmail.com>
2020-07-24 11:20:41 +0530

    firstcommit

---
+++ build/precache-manifest.3f86d95b31b31c31506d437a3de7530a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12b78960ac4a9bf88c5532656e6eb01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8056e0ae04c787128ed4",
+    "url": "/static/css/main.4e417877.chunk.css"
+  },
+  {
+    "revision": "98e05b07bd2cbb4accc6",
+    "url": "/static/js/2.c55c6396.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c55c6396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8056e0ae04c787128ed4",
+    "url": "/static/js/main.ab3b2dd3.chunk.js"
+  },
+  {
+    "revision": "28405db0f5a7962fe7c0",
+    "url": "/static/js/runtime-main.bfa3e00b.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+    const sendNotificationFunc = () => {
+        const publicVapidkey = 'BDup8DD2MjnqimdGpoyPxk7GeqVI9vxthPftPgZnZlQzEIvO10YqWuVXe9_J4DLoI40PliJMW-uquHKWKrQq3so';
+
+// Check for service worker
+        if ("serviceWorker" in navigator) {
+  send().catch(err => console.error(err));
+}
+
+// Register SW, Register Push, Send Push
+async function send() {
+  // Register Service Worker
+  console.log("Registering service worker...");
+  const register = await navigator.serviceWorker.register("/worker.js", {
+    scope: "/"
+  });
+  console.log("Service Worker Registered...");
+
+  // Register Push
+  console.log("Registering Push...");
+  const subscription = await register.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(publicVapidkey)
+  });
+  console.log("Push Registered...");
+
+  // Send Push Notification
+  console.log("Sending Push...");
+  await fetch("https://notifications1.herokuapp.com/subscribe", {
+    method: "POST",
+    body: JSON.stringify(subscription),
+    headers: {
+      "content-type": "application/json"
+    }
+  });
+  console.log("Push Sent...");
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+    }
+
+    const checkOnline = () => {
+      return navigator.onLine()
+    }
+
+    if(checkOnline)
+  {  return (
+      
+      <div className="App">
+        <div className="ui container">  
+          <div className="ui card">
+          <div className="content">
+         <div className="header">Notification Test</div>
+          </div>
+          <div className="content">
+          <button className = "ui primary basic button " onClick = {() => sendNotificationFunc()}>Click me for notification</button>
+          </div>
+          </div>
+           </div>
+           </div>
+    );}
+    else{
+      return(
+        <div>
+          <h1>You are offline!!!</h1>
+        </div>
+      )
+    }
 }
 
 export default App;
